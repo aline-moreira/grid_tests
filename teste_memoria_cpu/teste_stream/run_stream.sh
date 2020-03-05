098285140b13a30fabe22b91afff25cb4ade5cd4
aline <alinesmoreiraicloud@gmail.com>
2020-03-05 23:51:14 +0000

    updated run stream with cpu limit

--- teste_memoria_cpu/teste_stream/run_stream.sh
+++ teste_memoria_cpu/teste_stream/run_stream.sh
@@ -1,10 +1,23 @@
-for TAM in {3560000..999999999..90000000}
-	do echo $TAM
-	rm stream && gcc -O2 -fopenmp -DNTIMES=20 -DSTREAM_ARRAY_SIZE=$TAM -mcmodel=medium stream.c -o stream && (./stream & ( echo "RUNNING PSRECORD ON $! PROCESS" && psrecord $! --interval 0.1 --log stream_memory_$TAM.png --include-children))
-done
-for TAM in {3560000..999999999..90000000} do
-	for LIMIT in {10..100..10} do
-		echo "RUNNING STREAM WITH $TAM SIZE AND CPU LIMIT WITH $LIMIT"
-		rm stream && gcc -O2 -fopenmp -DNTIMES=20 -DSTREAM_ARRAY_SIZE=$TAM -mcmodel=medium stream.c -o stream && (./stream & (PROC=$! && (echo "RUNNING PSRECORD ON $PROC PROCESS" && psrecord $! --interval 0.1 --log stream_memory_$TAM_limit_$LIMIT.png --include-children) & cpulimit -l $LIMIT -p $PROC))
-	done
-done
+#!/bin/bash
+for (( TAM = 356000000; TAM <= 1436000000; TAM = TAM + 90000000)); do 
+	echo "RUNNING STREAM WITH $TAM SIZE";
+	rm -f stream;
+	gcc -O2 -fopenmp -DNTIMES=20 -DSTREAM_ARRAY_SIZE=$TAM -mcmodel=medium stream.c -o stream;
+        ./stream & 
+	echo "STREAM PROCESS PID $!" && PROC=$!;
+	echo "RUNNING PSRECORD ON $PROC PROCESS" && psrecord $PROC --interval 0.1 --log stream_memory_$TAM.png --include-children;
+done;
+
+for (( TAM = 356000000; TAM <= 1436000000; TAM = TAM + 90000000)); do
+	for (( LIMIT = 10; LIMIT <= 100; LIMIT = LIMIT + 10)); do
+		echo "RUNNING STREAM WITH $TAM SIZE AND CPU LIMIT WITH $LIMIT";
+		rm -f stream;
+	       	gcc -O2 -fopenmp -DNTIMES=20 -DSTREAM_ARRAY_SIZE=$TAM -mcmodel=medium stream.c -o stream;
+	        ./stream &
+	       	echo "STREAM PROCESS PID $!" && PROC=$!;
+		echo "RUNNING PSRECORD ON $PROC PROCESS" && psrecord $PROC --interval 0.1 --log stream-memory-$TAM-limit-$LIMIT.log --include-children &
+	       	echo "RUNNING CPULIMIT ON $PROC PROCESS" && cpulimit -l $LIMIT -p $PROC;
+	done;
+done;
+
+rm -f stream;
