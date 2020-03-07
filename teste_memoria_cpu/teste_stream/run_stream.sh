0371ab3bf18aa3bc9ab475fa0d5624a806adeaea
Aline Moreira <alinesmoreiraicloud@gmail.com>
2020-03-07 11:39:32 +0100

    stream results with and without limiting the cpu usage

--- teste_memoria_cpu/teste_stream/run_stream.sh
+++ teste_memoria_cpu/teste_stream/run_stream.sh
@@ -5,17 +5,17 @@ for (( TAM = 356000000; TAM <= 1436000000; TAM = TAM + 90000000)); do
 	gcc -O2 -fopenmp -DNTIMES=20 -DSTREAM_ARRAY_SIZE=$TAM -mcmodel=medium stream.c -o stream;
         ./stream & 
 	echo "STREAM PROCESS PID $!" && PROC=$!;
-	echo "RUNNING PSRECORD ON $PROC PROCESS" && psrecord $PROC --interval 0.1 --log stream_memory_$TAM.png --include-children;
+	echo "RUNNING PSRECORD ON $PROC PROCESS" && psrecord $PROC --interval 0.1 --log logs/stream_memory_$TAM.log --include-children;
 done;
 
-for (( TAM = 356000000; TAM <= 1436000000; TAM = TAM + 90000000)); do
-	for (( LIMIT = 10; LIMIT <= 100; LIMIT = LIMIT + 10)); do
+for (( LIMIT = 25; LIMIT <= 100; LIMIT = LIMIT + 25)); do
+	for (( TAM = 356000000; TAM <= 1436000000; TAM = TAM + 90000000)); do
 		echo "RUNNING STREAM WITH $TAM SIZE AND CPU LIMIT WITH $LIMIT";
 		rm -f stream;
 	       	gcc -O2 -fopenmp -DNTIMES=20 -DSTREAM_ARRAY_SIZE=$TAM -mcmodel=medium stream.c -o stream;
 	        ./stream &
 	       	echo "STREAM PROCESS PID $!" && PROC=$!;
-		echo "RUNNING PSRECORD ON $PROC PROCESS" && psrecord $PROC --interval 0.1 --log stream-memory-$TAM-limit-$LIMIT.log --include-children &
+		echo "RUNNING PSRECORD ON $PROC PROCESS" && psrecord $PROC --interval 0.1 --log logs/stream-memory-$TAM-limit-$LIMIT.log --include-children &
 	       	echo "RUNNING CPULIMIT ON $PROC PROCESS" && cpulimit -l $LIMIT -p $PROC;
 	done;
 done;
