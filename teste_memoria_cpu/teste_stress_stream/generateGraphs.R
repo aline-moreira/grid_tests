a674c13050a95edbc43a9e1092589a41b3a8e39f
aline moreira <alinesmoreiraicloud@gmail.com>
2020-03-21 23:51:47 -0300

    update v2 test

--- teste_memoria_cpu/teste_stress_stream/generateGraphs.R
+++ teste_memoria_cpu/teste_stress_stream/generateGraphs.R
@@ -15,12 +15,12 @@ library(dplyr)
 
 pallet_colors <- c("#00AFBB", "#E7B800", "#FC4E07","#52854C","#FFDB6D","#4E84C4")
 
-times<- read.table("times_n_pinado.log",header=TRUE,sep=";")
+times<- read.table("tempos_testes.times",header=TRUE,sep=";")
 times$start <- as.POSIXct(times$start,tz="UTC",  format="%c")
 times$end <- as.POSIXct(times$end,tz="UTC",  format="%c")
 
 ##########GETTING THE INFO OF JSON######################
-energy <- fromJSON(file="energy_1922174.json")
+energy <- fromJSON(file="energy_1922237.json")
 
 energy$items[[1]]$timestamps = as.POSIXct(energy$items[[1]]$timestamps, origin="1970-01-01",tz="UTC")
 
@@ -39,37 +39,50 @@ stream4 <- energy %>% filter(
     energy$tempo >= times$start[times$test=="STREAM" & times$cpu==4] & energy$tempo <= times$end[times$test=="STREAM"& times$cpu==4]
 )
 stream4$cpu <- 4
+stream4$ram <- 32
 stream4$test <- "stream"
+q <- quantile(stream4$cpu, c(0.1, 0.9))
+stream4 <- stream4[stream4$cpu >= q[1] & stream4$cpu <= q[2], ]
+
 
 stream128 <- energy %>% filter(
     energy$tempo >= times$start[times$test=="STREAM" & times$cpu==128] & energy$tempo <= times$end[times$test=="STREAM"& times$cpu==128]
 )
 stream128$cpu <- 128
+stream128$ram <- 754
 stream128$test <- "stream"
+q <- quantile(stream128$cpu, c(0.1, 0.9))
+stream128 <- stream128[stream128$cpu >= q[1] & stream128$cpu <= q[2], ]
 
 stress4 <- energy %>% filter(
     energy$tempo >= times$start[times$test=="STRESS" & times$cpu==4] & energy$tempo <= times$end[times$test=="STRESS"& times$cpu==4]
 )
 stress4$cpu <- 4
+stress4$ram <- 32
 stress4$test <- "stress"
+q <- quantile(stress4$cpu, c(0.1, 0.9))
+stress4 <- stress4[stress4$cpu >= q[1] & stress4$cpu <= q[2], ]
 
 stress128 <- energy %>% filter(
     energy$tempo >= times$start[times$test=="STRESS" & times$cpu==128] & energy$tempo <= times$end[times$test=="STRESS"& times$cpu==128]
 )
 stress128$cpu <- 128
+stress128$ram <- 754
 stress128$test <- "stress"
+q <- quantile(stress128$cpu, c(0.1, 0.9))
+stress128 <- stress128[stress128$cpu >= q[1] & stress128$cpu <= q[2], ]
 
 dt_tests_4 <- rbind(stream4,stress4)
 dt_tests_128 <- rbind(stream128,stress128)
 
 diff <- data.frame(
-        stream4=c("stream", 4, mean(stream4$consumo)),
-        stream128=c("stream", 128, mean(stream128$consumo)),
-        stress4=c("stress", 4, mean(stress4$consumo)),
-        stress128=c("stress", 128, mean(stress128$consumo))
+    stream4=c("stream", 4, 32, mean(stream4$consumo)),
+    stream128=c("stream", 128, 754, mean(stream128$consumo)),
+    stress4=c("stress", 4, 32, mean(stress4$consumo)),
+    stress128=c("stress", 128, 754, mean(stress128$consumo))
 )
 
-rownames(diff) <- c("teste","cpu","consumo")
+rownames(diff) <- c("teste","cpu","ram","consumo")
 diff <- as.data.frame(t(diff))
 
 diff$consumo <- as.numeric(levels(diff$consumo))
@@ -78,11 +91,6 @@ diff$consumo <- as.numeric(levels(diff$consumo))
     
 #diff$cpu <- as.numeric(diff$cpu)
 #diff$consumo <- as.numeric(diff$consumo)
-
-rm(stress4)
-rm(stress128)
-rm(stream4)
-rm(stream128)
 
 tiff("diff_4.tiff", width= 3600, height= 2200, units="px", res=400,compression = 'lzw')
 p1 <- ggplot(data=dt_tests_4, aes(x=tempo, y=consumo, color=test))+
@@ -153,7 +161,7 @@ all <- rbind(dt_tests_4, dt_tests_128)
 
 tiff("diff_all.tiff", width= 3600, height= 2200, units="px", res=400,compression = 'lzw')
 p3 <- ggplot(data=all, aes(x=as.factor(cpu), y=consumo, color=test))+
-    geom_boxplot(outlier.colour="black", outlier.shape=16, outlier.size=2, notch=FALSE)+ 
+    geom_boxplot(outlier.colour="black", outlier.shape=16, outlier.size=2, notch=FALSE)+
     #geom_bar(stat = "identity", position=position_dodge())+
     theme_classic()+
     theme(
@@ -179,15 +187,17 @@ p3 <- ggplot(data=all, aes(x=as.factor(cpu), y=consumo, color=test))+
         x="CPU",
         y="Consumo (W/s)",
         color= "Benchmark"
-    )+ 
+    )+
     scale_y_continuous(limits=c(0,750), breaks=seq(0,750,50))
 
 plot(p3)
 dev.off()
 
+
 rm(p1)
 rm(p2)
 rm(p3)
+
 system("for f in *.tiff; do convert -trim $f ${f%.*}.png; done;")
 #system("mv *.png graphs")
 system("rm *.tiff")
