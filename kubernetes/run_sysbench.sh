6ad319d3392883c8dc6c3717ffb6079874cf93b3
Aline Moreira <alinesmoreiraicloud@gmail.com>
2020-04-01 04:52:52 +0200

    kubernetes scripts

---
+++ kubernetes/run_sysbench.sh
@@ -0,0 +1,11 @@
+for cpus in 1 2 4 8 16 32 64 128; do
+#Iniciar o deploy, dorme por 10 minutos e depois mata os pods
+	START=`date -u` 
+	kubectl apply --namespace testes -f sysbench/sysbench-$cpus.yaml 
+	sleep 600; 
+	kubectl delete --namespace testes deployment/sysbench-$cpus ;
+	END=`date -u`;
+	echo "$cpus;$START;$END" >> sysbench/kubernetes.times;
+	sleep 90;
+done
+
