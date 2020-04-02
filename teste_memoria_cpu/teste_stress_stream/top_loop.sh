20556ccedd0436d464679c99e7d352b20faa81d6
aline moreira <alinesmoreiraicloud@gmail.com>
2020-04-02 15:44:58 -0300

    clean master branch

--- teste_memoria_cpu/teste_stress_stream/top_loop.sh
+++
@@ -1,7 +0,0 @@
-if [[ $# -ne 1 ]]; then
-	echo "Illegal number of parameters (FILE NAME)"
-	exit 2
-fi
-while true; do
-	top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9. │]*\)%* id.*/\1/" | awk '{print (100 - $1)}' >> $1
-done;
