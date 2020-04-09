08d7a91c43eea39e2c86279b71142dd7ef791026
Aline Moreira <alinesmoreiraicloud@gmail.com>
2020-04-09 04:26:45 +0200

    network multiclient version 2 test

--- iperf/multiple_host/run_iperf_host_client.sh
+++ iperf/multiple_host/run_iperf_host_client.sh
@@ -7,14 +7,12 @@ VERSION=$1;
 
 echo "client;`hostname`" >> $VERSION/nodes.types
 
-TIME=60;
+TIME=600;
 ip_iperf=`cat ip_server`;
 echo $ip_iperf;
 
-for clients in 1; do
-	for band in 1M; do
-#for clients in 1 10 50 100; do
-#	for band in 1M 10M 100M 1000M; do
+for clients in 1 10 50 100; do
+	for band in 1M 10M 100M 1000M; do
 		echo "RUNNING $clients CLIENTS WITH $band"
 		START=`date -u`;
 		docker run --name iperf3-client -itd --network host networkstatic/iperf3 -c $ip_iperf -b $band -t $TIME -P $clients;
