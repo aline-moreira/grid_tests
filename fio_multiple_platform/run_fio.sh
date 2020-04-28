44edd07a4bda66a19dbbfec15fd2a2ff7d25340f
aline moreira <alinesmoreiraicloud@gmail.com>
2020-04-27 23:19:34 -0300

    updated fio benchmark run script

--- fio_multiple_platform/run_fio.sh
+++ fio_multiple_platform/run_fio.sh
@@ -6,27 +6,47 @@ fi
 PLATFORM=$1;
 VERSION=$2;
 
-ls $VERSION | mkdir $VERSION;
-ls $VERSION/logs | mkdir $VERSION/logs;
+ls $VERSION || mkdir $VERSION;
+ls $VERSION/logs || mkdir $VERSION/logs;
+ls fio-data || mkdir fio-data;
 
 #if [ "$PLATFORM" = "host" ] || [ "$PLATFORM" = "vm" ]; then
 #	sudo-g5k apt-get install fio -y
 #fi
 
 for i in 1 5 10 50; do
+	echo "
+[global]
+randrepeat=1
+direct=1
+gtod_reduce=1
+name=test
+filename=test
+bs=32k
+readwrite=randread
+
+[teste_fio_${i}G_${PLATFORM}]
+size=${i}G
+ioengine=libaio
+iodepth=64
+	" > ./fio-data/${i}G.fio;
+
 	START=`date -u`;
+	
 	if [ "$PLATFORM" = "host" ] || [ "$PLATFORM" = "vm" ]; then
-		fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --readwrite=randread --directory=./ --output=./$VERSION/logs/teste_fio_"${i}"G_"${PLATFORM}".log --size="${i}"G;
+		fio fio-data/${i}G.fio --output=./$VERSION/logs/teste_fio_"${i}"G_"${PLATFORM}".log;
+		rm -f test
 	fi
 	if [ "$PLATFORM" = "docker" ]  || [ "$PLATFORM" = "vm_docker" ]; then
-		docker run --rm -v `pwd`/:/root/fio ljishen/fio  --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --readwrite=randread --directory=/root/fio --output=/root/fio/$VERSION/logs/teste_fio_"${i}"G_"${PLATFORM}".log --size="${i}"G;
-		`cat /root/fio/$VERSION/logs/teste_fio_"${i}"G_"${PLATFORM}".log --size="${i}"G`;
+		docker run --rm -v `pwd`/fio-data:/tmp/fio-data -e JOBFILES=${i}G.fio  clusterhq/fio-tool > $VERSION/logs/teste_fio_"${i}"G_"${PLATFORM}".log;
 	fi
+
 	END=`date -u`;
 	echo "$PLATFORM;$i;$START;$END" >> $VERSION/fio.times;
 	sleep 60;
 done;
 
+rm -r fio-data
 START=`date -u`;
 sleep 600;
 END=`date -u`;
