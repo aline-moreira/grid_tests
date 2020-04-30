cd04e0f4b0b8cf6e4af28f3e00ce6cd707d4e365
aline moreira <alinesmoreiraicloud@gmail.com>
2020-04-30 15:51:32 -0300

    Update docker install

--- create_vm/install_docker.sh
+++ create_vm/install_docker.sh
@@ -2,7 +2,7 @@ apt-get update -y
 
 apt-get install git apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
 apt-key fingerprint 0EBFCD88 
 
