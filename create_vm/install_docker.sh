719b070ce305cb435264d7057260c0dbe41e6d15
aline moreira <alinesmoreiraicloud@gmail.com>
2020-04-30 15:51:48 -0300

    Update docker install

--- create_vm/install_docker.sh
+++ create_vm/install_docker.sh
@@ -8,6 +8,6 @@ apt-key fingerprint 0EBFCD88
 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
-apt-get update
+apt-get update -y
 
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install docker-ce docker-ce-cli containerd.io -y
