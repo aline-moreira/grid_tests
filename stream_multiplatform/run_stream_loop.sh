50b9ce44cffc12a9c217eca2f4ff277cb44282e0
aline moreira <alinesmoreiraicloud@gmail.com>
2020-05-13 20:27:58 -0300

    update

--- stream_multiplatform/run_stream_loop.sh
+++ stream_multiplatform/run_stream_loop.sh
@@ -10,7 +10,7 @@ ARRAY=("$@");
 mkdir $VERSION;
 
 echo "VERSION $VERSION";
-echo "RUNNING OPTIONS $ARRAY";
+echo "RUNNING OPTIONS ${ARRAY[@]}";
 
 for RAM in "${ARRAY[@]}"; do	
 	INIT=`date -u`;
@@ -19,11 +19,12 @@ for RAM in "${ARRAY[@]}"; do
 	echo "RUNNING STREAM WITH $RAM GB OF RAM (TOTAL $TAM)";
 	rm -f stream;
 	gcc -O2 -fopenmp -DNTIMES=20 -DSTREAM_ARRAY_SIZE=$TAM -mcmodel=medium stream.c -o stream;
+	export OMP_NUM_THREADS=1;
+	sleep 10;
 	./stream & echo "STREAM PROCESS PID $!" && BENCH_PROC=$!;
 	END=`date -u`;
 	echo "Finish the test in time $END";
 	echo "STREAM;$PLATFORM;$RAM;$INIT;$END" >> $VERSION/benchmarks.time;
-	rm stream;
 	sleep 60;
 done;
 
@@ -31,3 +32,4 @@ INIT=`date -u`;
 sleep 600;
 END=`date -u`;
 echo "STREAM;$PLATFORM;0;$INIT;$END" >> $VERSION/benchmarks.time;
+rm -f stream;
