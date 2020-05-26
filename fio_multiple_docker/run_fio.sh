64b7952f82a24925fa747197a6c8ffba7295759e
aline moreira <alinesmoreiraicloud@gmail.com>
2020-05-26 17:17:32 -0300

    update

--- fio_multiple_docker/run_fio.sh
+++ fio_multiple_docker/run_fio.sh
@@ -1,20 +1,15 @@
-if [ "$#" -ne 4 ]; then
-	echo "Illegal number of parameters (PLATFORM, VERSION_TEST, IO, DOCKERS)";
+if [ "$#" -ne 3 ]; then
+	echo "Illegal number of parameters (VERSION_TEST, IO, DOCKERS)";
 	exit 2;
 fi
 
-PLATFORM=$1;
-VERSION=$2;
-IO=$3;
-DOCKERS=$4;
+VERSION=$1;
+IO=$2;
+DOCKERS=$3;
 
 ls $VERSION || mkdir $VERSION;
 ls $VERSION/logs || mkdir $VERSION/logs;
 ls fio-data || mkdir fio-data;
-
-#if [ "$PLATFORM" = "host" ] || [ "$PLATFORM" = "vm" ]; then
-#	sudo-g5k apt-get install fio -y
-#fi
 
 for i in $IO; do
 	echo "
@@ -28,7 +23,7 @@ filename=test
 bs=32k
 readwrite=randread
 
-[teste_fio_${i}G_${PLATFORM}]
+[teste_fio_${i}G_docker]
 size=${i}G
 ioengine=libaio
 iodepth=64
@@ -40,16 +35,10 @@ iodepth=64
 
 	START=`date -u`;
 	
-	if [ "$PLATFORM" = "host" ] || [ "$PLATFORM" = "vm" ]; then
-		fio fio-data/${i}G.fio --output=./$VERSION/logs/teste_fio_"${i}"G_"${PLATFORM}".log;
-		rm -f test
-	fi
-	if [ "$PLATFORM" = "docker" ]  || [ "$PLATFORM" = "vm_docker" ]; then
-		docker run --rm -v `pwd`/fio-data:/tmp/fio-data -e JOBFILES="${i}"G.fio  clusterhq/fio-tool > $VERSION/logs/teste_fio_"${i}"G_"${PLATFORM}".log;
-	fi
-
+	docker run --rm -v `pwd`/fio-data:/tmp/fio-data -e JOBFILES="${i}"G.fio  clusterhq/fio-tool > $VERSION/logs/teste_fio_"${i}"G_"${DOCKERS}docker".log;
+	
 	END=`date -u`;
-	echo "$PLATFORM;$i;$DOCKERS;$START;$END" >> $VERSION/fio.times;
+	echo "docker;$i;$DOCKERS;$START;$END" >> $VERSION/fio.times;
 	sleep 60;
 done;
 
@@ -57,6 +46,6 @@ rm -r fio-data
 START=`date -u`;
 sleep 600;
 END=`date -u`;
-echo "$PLATFORM;0;$DOCKERS;$START;$END" >> $VERSION/fio.times;
+echo "docker;0;$DOCKERS;$START;$END" >> $VERSION/fio.times;
 
 #curl -kn https://api.grid5000.fr/stable/sites/grenoble/metrics/power/timeseries/?job_id="${OAR_JOB_ID}" > ./$1/energy_"${OAR_JOB_ID}".json;
